9162008b71c25d5e855e47583a3a34dee6d1618c
Sher208 <thilakrajshetty11@gmail.com>
2023-01-22 21:46:30 +0530

    Added signout Test cases

--- auth/src/routes/__test__/signout.test.ts
+++ auth/src/routes/__test__/signout.test.ts
@@ -10,10 +10,7 @@ it("clears the cookie after signing out", async () => {
     })
     .expect(201);
 
-  const response = await request(app)
-    .get("/api/users/signout")
-    .send({})
-    .expect(200);
+  const response = await request(app).get("/api/users/signout").expect(200);
 
   expect(response.get("Set-Cookie")[0]).toEqual(
     "session=; path=/; expires=Thu, 01 Jan 1970 00:00:00 GMT; httponly"
